a6cd82ba26b57842d4fda01b9fdc12f78198b718
Inna949Festchuk <innabori@mail.ru>
2022-11-21 23:58:57 +0200

    Home_task_ok

--- Task_2.py
+++ Task_2.py
@@ -1,7 +1,7 @@
 import requests
 import os
 
-TOKEN = "y0_AgAAAAAEjD-yAADLWwAAAADTV-dh-RUAV19UQjOg6ncEDLe5v3S-CZs"
+# TOKEN = "..."
 
 class YaUploader:
     def __init__(self, token: str):
@@ -15,18 +15,18 @@ class YaUploader:
 
     def upload(self, file_path: str):
         """Метод загружает файл file в папку на яндекс диск"""
-        
         # Create folder on Yadisk
         upload_url_folder = "https://cloud-api.yandex.net/v1/disk/resources"
         headers = self.get_headers()
         response_folder = requests.put(f'{upload_url_folder}?path={path_folder}', headers=headers)
-        
         # Upload file to folder on Yadisk
+        # Зарезервировать ссылку для загрузки файла
         upload_url_file = "https://cloud-api.yandex.net/v1/disk/resources/upload"
         disk_file_path = f'{path_folder}/{os.path.basename(path_to_file)}'
         params = {"path": disk_file_path, "overwrite": "true"}  
         response = requests.get(upload_url_file, headers=headers, params=params) 
         result = response.json()
+        # Загрузить файл по полученной ссылке
         href = result.get("href", "")
         response = requests.put(href, data=open(path_to_file, 'rb')) 
         if response.status_code == 201:
